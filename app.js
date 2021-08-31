6377abccabeadf7e90dd9f14c704d13815c4a54a
Mobashir Farhan <farhanmobashir007@gmail.com>
2021-08-31 23:56:56 +0530

    adds all the basic functionality to the app

---
+++ app.js
@@ -0,0 +1,40 @@
+const birthdateInput = document.querySelector(".birthdate-input");
+
+const checkButton = document.querySelector(".check-button");
+
+const output = document.querySelector(".output");
+
+let userBirthdate = "";
+
+birthdateInput.addEventListener("change", function (e) {
+  userBirthdate = e.target.value;
+});
+
+let formattedDate = "";
+
+function isPalindrome(str) {
+  let temp = userBirthdate.split("-");
+  formattedDate = temp;
+  str = temp[2] + temp[1] + temp[0];
+  let str2 = str.split("").reverse().join("");
+  if (str === str2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isPalindrome("10-02-2001"));
+
+checkButton.addEventListener("click", function () {
+  if (userBirthdate !== "" && isPalindrome(userBirthdate) === true) {
+    console.log(formattedDate);
+    output.innerText = `Wooho your birthdate ${
+      formattedDate[2] + "-" + formattedDate[1] + "-" + formattedDate[0]
+    }  is a palindrome`;
+  } else if (isPalindrome(userBirthdate) === false) {
+    output.innerText = `Sorry , your birthdate ${
+      formattedDate[2] + "-" + formattedDate[1] + "-" + formattedDate[0]
+    } is not a palindrome`;
+  }
+});
